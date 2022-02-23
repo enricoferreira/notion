af9578a322e7bf2ddb26efe6eae26cd12bd06576
Enrico Ferreira <enricoferreira41@gmail.com>
2022-02-23 17:33:03 -0300

    Logout

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,10 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
+    <v-app-bar v-if="$route.path != '/login' && $route.path != '/register'" app color="primary" dark>
       <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+      <v-btn text @click="logout">
+        <v-icon left>mdi-logout</v-icon>
+        Logout
       </v-btn>
     </v-app-bar>
 
@@ -47,6 +18,17 @@
 
 export default {
   name: 'App',
+  methods:{
+    logout(){
+      this.$store.state.auth.signOut().then(() => {
+        console.log('Usuário deslogado com sucesso!');
+        // Sign-out successful.
+      }).catch((error) => {
+        console.log('Falha ao desloga usuário!', error);
+        // An error happened.
+      });
+    }
+  },
   beforeMount(){
     this.$store.state.auth.onAuthStateChanged((user) => {
       if (!user) {
